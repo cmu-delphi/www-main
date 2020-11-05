56b4e5650802024f43d26ae8b8823cc5ef9dc895
Samuel Gratzl <sam@sgratzl.com>
2020-11-05 12:38:41 -0500

    build: try building it on netlify

---
+++ netlify_build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get install -y pandoc r-base
+
+# install dependencies
+Rscript ./R/prepare.R
+
+# build Rmd files
+Rscript -e 'blogdown::build_site(run_hugo=FALSE, build_rmd=TRUE)'
+
+# run hugo
+hugo -b $DEPLOY_PRIME_URL --gc --minify
