9a20ae04e17b67530a90fd4d7d446c49954aebce
Samuel Gratzl <sam@sgratzl.com>
2020-11-24 09:51:14 -0500

    build: configure prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,4 +3,19 @@ module.exports = {
   semi: true,
   singleQuote: true,
   trailingComma: 'es5',
+
+  overrides: [
+    {
+      files: ['*.html'],
+      options: {
+        parser: 'go-template',
+      },
+    },
+    {
+      files: ['*.Rmd'],
+      options: {
+        parser: 'markdown',
+      },
+    },
+  ],
 };
