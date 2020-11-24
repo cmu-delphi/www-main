23432dc089528978c6db983e2e2fb388fbe71933
Samuel Gratzl <sam@sgratzl.com>
2020-11-24 16:47:10 -0500

    feat: blog toc

--- themes/delphi/assets/js/blog/index.js
+++ themes/delphi/assets/js/blog/index.js
@@ -2,8 +2,10 @@ import hljs from 'highlight.js';
 import renderMathInElement from 'katex/dist/contrib/auto-render.mjs';
 import { initializeCodeFolding } from './codeFolding';
 import imageModal from './imageModal';
+import initializeTableOfContent from './toc';
 
 hljs.initHighlightingOnLoad();
 initializeCodeFolding();
 Array.from(document.querySelectorAll('.math')).forEach((elem) => renderMathInElement(elem));
 imageModal();
+initializeTableOfContent();
