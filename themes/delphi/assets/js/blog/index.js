960a694715151035d743d2c2a0d1e58fa8176fa5
Samuel Gratzl <sam@sgratzl.com>
2020-11-24 14:39:52 -0500

    feat: full screen popup for blog images

--- themes/delphi/assets/js/blog/index.js
+++ themes/delphi/assets/js/blog/index.js
@@ -1,7 +1,9 @@
 import hljs from 'highlight.js';
 import renderMathInElement from 'katex/dist/contrib/auto-render.mjs';
 import { initializeCodeFolding } from './codeFolding';
+import imageModal from './imageModal';
 
 hljs.initHighlightingOnLoad();
 initializeCodeFolding();
 Array.from(document.querySelectorAll('.math')).forEach((elem) => renderMathInElement(elem));
+imageModal();
