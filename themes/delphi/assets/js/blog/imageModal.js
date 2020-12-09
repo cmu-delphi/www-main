52e1e09ea5a9176c8332fb02af4d9d0709808a6c
Samuel Gratzl <sam@sgratzl.com>
2020-12-09 08:57:14 -0500

    feat: add wide flag for figures

--- themes/delphi/assets/js/blog/imageModal.js
+++ themes/delphi/assets/js/blog/imageModal.js
@@ -24,10 +24,11 @@ export default function imageModal() {
   refButton.title = "Show image in fullscreen";
   refButton.innerHTML = `<svg width="15" height="15" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" data-svg="expand"><polygon points="13 2 18 2 18 7 17 7 17 3 13 3"></polygon><polygon points="2 13 3 13 3 17 7 17 7 18 2 18"></polygon><path fill="none" stroke="#000" stroke-width="1.1" d="M11,9 L17,3"></path><path fill="none" stroke="#000" stroke-width="1.1" d="M3,17 L9,11"></path></svg>`;
 
-  Array.from(document.querySelectorAll(".blog-blog p > img")).forEach((elem) => {
+  Array.from(document.querySelectorAll(".blog-blog p > img, .blog-blog .figure > img")).forEach((elem) => {
     const button = refButton.cloneNode(true);
     elem.insertAdjacentElement("beforebegin", button);
     elem.parentElement.classList.add("blog-image-wrapper");
+    elem.className.split(' ').filter(Boolean).forEach((clazzName) => elem.parentElement.classList.add(clazzName));
     button.addEventListener("click", (e) => {
       e.preventDefault();
       e.target.blur();
